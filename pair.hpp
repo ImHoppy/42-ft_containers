b7d04c527ad77039aafe4675d5edda1e7eaf2b91
Marius BRAETS <mbraets@student.42.fr>
2023-01-17 12:28:20 +0100

    pair: Format and fix typo

--- pair.hpp
+++ pair.hpp
@@ -5,13 +5,13 @@
 
 namespace ft
 {
-	temaplate<typename T1, typename T2>
+	template <typename T1, typename T2>
 	pair<T1, T2> make_pair(T1 x, T2 y)
 	{
 		return (pair<T1, T2>(x, y));
 	}
 
-	template<typename T1, typename T2>
+	template <typename T1, typename T2>
 	struct pair
 	{
 		typedef T1 first_type;
@@ -19,16 +19,16 @@ namespace ft
 
 		T1 first;
 		T2 second;
-		
-		// Default constructor
-		pair() : first(T1()), second(T2()) {};
-		// nitialization constructor
-		pair(const first_type& a, const second_type& b) : first(a), second(b) {};
-		// Copy constructor
-		template<clas U, class V>
-		pair(const pair<U, V>& pr) : first(pr.first), second(pr.second) {};
 
-		pair& operator=(const pair& pr)
+		// Default constructor
+		pair() : first(T1()), second(T2()){};
+		// nitialization constructor
+		pair(const first_type &a, const second_type &b) : first(a), second(b){};
+		// Copy constructor
+		template <clas U, class V>
+		pair(const pair<U, V> &pr) : first(pr.first), second(pr.second){};
+
+		pair &operator=(const pair &pr)
 		{
 			if (this != &pr)
 			{
@@ -39,52 +39,52 @@ namespace ft
 		}
 
 		// Compare two pairs
-		template<class U, class V>
-		friend bool operator==(const pair<U, V>& lhs, const pair<U, V>& rhs);
-		template<class U, class V>
-		friend bool operator!=(const pair<U, V>& lhs, const pair<U, V>& rhs);
-		template<class U, class V>
-		friend bool operator<(const pair<U, V>& lhs, const pair<U, V>& rhs);
-		template<class U, class V>
-		friend bool operator<=(const pair<U, V>& lhs, const pair<U, V>& rhs);
-		template<class U, class V>
-		friend bool operator>(const pair<U, V>& lhs, const pair<U, V>& rhs);
-		template<class U, class V>
-		friend bool operator>=(const pair<U, V>& lhs, const pair<U, V>& rhs);
+		template <class U, class V>
+		friend bool operator==(const pair<U, V> &lhs, const pair<U, V> &rhs);
+		template <class U, class V>
+		friend bool operator!=(const pair<U, V> &lhs, const pair<U, V> &rhs);
+		template <class U, class V>
+		friend bool operator<(const pair<U, V> &lhs, const pair<U, V> &rhs);
+		template <class U, class V>
+		friend bool operator<=(const pair<U, V> &lhs, const pair<U, V> &rhs);
+		template <class U, class V>
+		friend bool operator>(const pair<U, V> &lhs, const pair<U, V> &rhs);
+		template <class U, class V>
+		friend bool operator>=(const pair<U, V> &lhs, const pair<U, V> &rhs);
 	};
 
-	template<class T1, class T2>
-	bool operator==(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	template <class T1, class T2>
+	bool operator==(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
 	{
 		return (lhs.first == rhs.first && lhs.second == rhs.second);
 	}
 
-	template<class T1, class T2>
-	bool operator!=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	template <class T1, class T2>
+	bool operator!=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
 	{
 		return (lhs.first != rhs.first || lhs.second != rhs.second);
 	}
 
-	template<class T1, class T2>
-	bool operator<(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	template <class T1, class T2>
+	bool operator<(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
 	{
 		return (lhs.first < rhs.first && lhs.second < rhs.second);
 	}
 
-	template<class T1, class T2>
-	bool operator<=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	template <class T1, class T2>
+	bool operator<=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
 	{
 		return (lhs.first <= rhs.first && lhs.second <= rhs.second);
 	}
 
-	template<class T1, class T2>
-	bool operator>(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	template <class T1, class T2>
+	bool operator>(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
 	{
 		return (lhs.first > rhs.first && lhs.second > rhs.second);
 	}
 
-	template<class T1, class T2>
-	bool operator>=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	template <class T1, class T2>
+	bool operator>=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
 	{
 		return (lhs.first >= rhs.first && lhs.second >= rhs.second);
 	}
