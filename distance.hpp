b4fa58f8ae31c49d3c635e569e694948a9fd35c7
Marius BRAETS <mbraets@student.42.fr>
2023-02-02 11:15:55 +0100

    Undo adding equal, duplicate

--- distance.hpp
+++ distance.hpp
@@ -31,31 +31,4 @@ namespace ft
 	{
 		return distance_tag(first, last, iterator_category(first));
 	}
-
-	template <class InputIterator1, class InputIterator2>
-	bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
-	{
-		while (first1 != last1)
-		{
-			if (!(*first1 == *first2))
-				return false;
-			++first1;
-			++first2;
-		}
-		return true;
-	}
-
-	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
-	bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
-	{
-		while (first1 != last1)
-		{
-			if (!pred(*first1, *first2))
-				return false;
-			++first1;
-			++first2;
-		}
-		return true;
-	}
-
 }
